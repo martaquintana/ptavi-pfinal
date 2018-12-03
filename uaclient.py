cb2856563263375e3fad41456b2aa610d5115c7b
Marta Quintana Portales <mquintan@beta14.aulas.gsyc.urjc.es>
2018-12-03 17:07:22 +0100

    Lee el fichero XML y hace una lista de diccionarios .

--- uaclient.py
+++ uaclient.py
@@ -4,8 +4,13 @@
 import sys
 import socket
 from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
 
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
 class XMLHandler(ContentHandler):
 
@@ -16,7 +21,7 @@ class XMLHandler(ContentHandler):
             "uaserver": ['ip', 'puerto'],
             "rtpaudio": ['puerto'],
             "regproxy": ['ip', 'puerto'],
-            "log": ['path']
+            "log": ['path'],
             "audio": ['path']
             }
         self.tag_list = []
@@ -31,10 +36,26 @@ class XMLHandler(ContentHandler):
             for atributo in self.diccionario[name]:
                 dic_tag[atributo] = attrs.get(atributo, "")
             self.tag_list.append(dic_tag)
+
     def get_tags(self):
-        """
-        Devuelve la lista de etiquetas,
-        sus atributos y el contenido de los atributos
-        """
         return self.tag_list
-       
+
+class XML:
+
+    def __init__(self, fichero):
+        self.get_tags = []
+        self.dicc = {}
+        parser = make_parser()
+        cHandler = XMLHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.get_tags = cHandler.get_tags()
+        print(self.get_tags)
+  
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    fichero = sys.argv[1]
+    leerxml = XML(fichero)
+   
--- uaserver.py
+++ uaserver.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
+import sys
+import socket
+from xml.sax import make_parser
+
+
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+from uaclient import XMLHandler
+
+class XML:
+
+    def __init__(self, fichero):
+        self.get_tags = []
+        self.dicc = {}
+        parser = make_parser()
+        cHandler = XMLHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.get_tags = cHandler.get_tags()
+        print(self.get_tags)
+  
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    fichero = sys.argv[1]
+    leerxml = XML(fichero)
+   
