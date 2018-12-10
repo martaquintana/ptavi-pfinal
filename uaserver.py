aa14804bf56c6dc5192508f44f296cbe86f37b28
Marta Quintana Portales <mquintan@gamma07.aulas.gsyc.urjc.es>
2018-12-10 10:48:17 +0100

    cambiar server al proxy.

--- uaserver.py
+++ uaserver.py
@@ -2,27 +2,72 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import socket
+import socketserver
+from uaclient import XMLHandler
+from uaclient import XML
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-from uaclient import XMLHandler
 
-class XML:
+class SIPHandler(socketserver.DatagramRequestHandler):
+    """SIP server class."""
 
-    def __init__(self, fichero):
-        self.get_tags = []
-        self.dicc = {}
-        parser = make_parser()
-        cHandler = XMLHandler()
-        parser.setContentHandler(cHandler)
-        parser.parse(open(fichero))
-        self.get_tags = cHandler.get_tags()
-        print(self.get_tags)
-  
+    def handle(self):
+        """Contesta a los diferentes metodos SIP que le manda el cliente."""
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+            linea_decod = line.decode('utf-8').split(" ")
+            print(len(linea_decod))
+            if (len(linea_decod) != 4 or 'sip:' not in linea_decod[1] or
+                    '@' not in linea_decod[1] or
+                    'SIP/2.0' not in linea_decod[2]):
+                        self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                        break
+
+            metodo = linea_decod[0]
+            if metodo == 'INVITE':
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                break
+
+            if metodo == 'ACK':
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
+                self.wfile.write(b"cancion.mp3 enviada")
+                break
+
+            if metodo == 'BYE':
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                break
+
+            else:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                break
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+
+
+
 if __name__ == "__main__":
     """
     Programa principal
     """
-    fichero = sys.argv[1]
-    leerxml = XML(fichero)
+    try:
+        fichero = sys.argv[1]
+        leerxml = XML(fichero)
+        DIC_CONFIG= XML.get_diccionario(leerxml)
+        print(DIC_CONFIG)
+        print(DIC_CONFIG['account_username'])
+        serv = socketserver.UDPServer((DIC_CONFIG['uaserver_ip'], int(DIC_CONFIG['uaserver_puerto']) ), SIPHandler)
+        print("Listening...")
+        try:
+            serv.serve_forever()
+        except KeyboardInterrupt:
+            print("Finalizado servidor")
+    except (IndexError, ValueError, PermissionError or len(sys.argv)< 2):
+        print("Usage: phython3 uaserver.py ua2.xml")
+
    
