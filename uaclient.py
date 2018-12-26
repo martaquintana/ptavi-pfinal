1390fb29ab992bc60091634dcfbd03ed142e2ad3
martaquintana <Marta@MacBook-Pro-de-Marta.local>
2018-12-26 17:07:27 +0100

    Corregido direccion de envio invite

--- uaclient.py
+++ uaclient.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
 
 
             if METODO == 'INVITE' or METODO == 'BYE':
-                line = (METODO + ' sip:' + DIC_CONFIG['account_username'] + ' SIP/2.0\r\n\r\n')
+                line = (METODO + ' sip:' + sys.argv[-1] + ' SIP/2.0\r\n\r\n')
                 print(line)
                 my_socket.send(bytes(line, 'utf-8'))
                 data = my_socket.recv(1024)
@@ -99,7 +99,7 @@ if __name__ == "__main__":
                 if METODO == 'INVITE':
                      line = ('Content-Type:' + ' application/sdp\r\n' + 'v=0\r\n'
 							+ 'o=' + DIC_CONFIG['account_username'] + DIC_CONFIG['regproxy_ip']+ '\r\n'
-							+ 's= misesion\r\n' + 't=0' + 'm=audio ' + DIC_CONFIG['rtpaudio_puerto']+ ' RTP\r\n')
+							+ 's= misesion\r\n' + 't=0\r\n' + 'm=audio ' + DIC_CONFIG['rtpaudio_puerto']+ ' RTP\r\n')
                      print("Enviando:", line)
                      my_socket.send(bytes(line, 'utf-8'))
                      data = my_socket.recv(1024)
